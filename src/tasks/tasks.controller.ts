8c50ba12e9c304e747d92862cba9193aa32046c7
Carlos Garcia Romero <garciacarlos78@gmail.com>
2020-09-02 20:55:29 +0200

    Add delete task endpoint

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -12,10 +13,19 @@ export class TasksController {
         return this.tasksService.getAllTasks();
     }
 
+    @Get(':id')
+    getTaskById(@Param('id') id:string) : Task {
+        return this.tasksService.getTaskById(id);
+    }
+
     @Post()
-    postTask(
-        @Body('title') title: string,
-        @Body('description') description: string) {
-            return this.tasksService.createTask(title, description);
+    postTask(@Body() createTaskDto: CreateTaskDto) : Task {
+            return this.tasksService.createTask(createTaskDto);
+    }
+
+    @Delete(':id')
+    deleteTaskById(@Param('id') id: string) : string {
+        if (this.tasksService.deleteTaskById(id)) return `Task with id ${id} deleted`;
+        else return `Task not found.`;
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import * as uuid from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -10,7 +11,13 @@ export class TasksService {
         return this.tasks;
     }
 
-    createTask(title: string, description: string) : Task {
+    getTaskById(id: string) : Task {
+        return this.tasks.find(task => task.id === id);
+    }
+
+    createTask(createTaskDto: CreateTaskDto) : Task {
+        const { title, description} = createTaskDto;
+
         const task: Task = {
             id: "id-"+Math.random(),
             title,
@@ -20,5 +27,15 @@ export class TasksService {
 
         this.tasks.push(task);
         return task;
+    }
+
+    deleteTaskById(id: string) : boolean {
+        const index = this.tasks.findIndex(task => task.id === id);
+        
+        if (index === -1) return false;
+        else {
+            this.tasks.splice(index, 1);
+            return true;
+        }
     }
 }
