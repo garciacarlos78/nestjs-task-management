5dcc73a316e3ce0212f2ec4290c31dae9c8453e2
Carlos Garcia Romero <garciacarlos78@gmail.com>
2020-09-04 14:44:48 +0200

    Added add task using repository

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -51,17 +51,10 @@ export class TasksService {
     }
     */
    
-    async createTask(createTaskDto: CreateTaskDto) :Promise<Task> {
-        const { title, description } = createTaskDto;
-
-        const task = new Task();
-        task.title = title;
-        task.description = description;
-        task.status = TaskStatus.OPEN;
-        await task.save();
-
-        return task;
+    createTask(createTaskDto: CreateTaskDto) :Promise<Task> {
+        return this.taskRepository.createTask(createTaskDto);
     }
+
     /*
     createTask(createTaskDto: CreateTaskDto): Task {
         const { title, description } = createTaskDto;
