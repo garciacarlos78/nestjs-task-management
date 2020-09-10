059abb2159a647b8de4897dc89a3f18e84d9fa87
Carlos Garcia Romero <garciacarlos78@gmail.com>
2020-09-10 13:28:27 +0200

    Refactor UserEntity tests

--- src/auth/user.entity.spec.ts
+++ src/auth/user.entity.spec.ts
@@ -1,43 +1,34 @@
-import { Test } from "@nestjs/testing";
 import { User } from './user.entity';
 import * as bcrypt from 'bcrypt';
 
-
 describe('UserEntity tests', () => {
 
-    let user;
+    let user: User;
 
     beforeEach(async () => {
-        const module = await Test.createTestingModule({
-            providers: [User]
-        }).compile();
-
-        user = module.get<User>(User);
-
         // mock user 
-        user.password = 'user password';
-        user.salt = 'user salt'
-
+        user = new User();
+        user.password = 'user test password';
+        user.salt = 'user test salt';
         // mock hash method of bcrypt
         bcrypt.hash = jest.fn();
     });
 
-
     describe('validatePassword test', () => {
 
         // test for a correct password
-        it('returns true when called with valid password', () => {
+        it('returns true when called with valid password', async () => {
             // mock hash resolved value to user's password
             bcrypt.hash.mockResolvedValue(user.password);
-
             // ensure bcrypt.hash has not been called before
-            expect(bcrypt.hash).not.toHaveBeenCalled();
+            expect(bcrypt.hash).not.toHaveBeenCalled();            
             // call method
-            const result = user.validatePassword(user.password);
+            const callingPassword = 'no matters which password, we are mocking the result';
+            const result = await user.validatePassword(callingPassword);
             // expect bcrypt.hash been called with expected parameters
-            expect(bcrypt.hash).toHaveBeenCalledWith(user.password, user.salt);
+            expect(bcrypt.hash).toHaveBeenCalledWith(callingPassword, user.salt);
             // expect result resolves to true
-            expect(result).resolves.toEqual(true);
+            expect(result).toEqual(true);
         });
 
         // test for a wrong password
